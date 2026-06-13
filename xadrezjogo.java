59c7e68c48281986b66268cfc0d6f90b0f880ea9
joao pedro <joaopedrogonzaga0@gmail.com>
2026-06-12 21:21:51 -0300

    tabuleiro de xadrez

--- xadrezjogo.java
+++ xadrezjogo.java
@@ -1,19 +1,9 @@
 public class xadrezjogo {
     public static void main(String[] args) {
-        Board board = new Board(8, 8);
-        King king = new King(board, Color.WHITE);
-
-        Position kingPos = new Position(4, 4);
-        board.placePiece(king, kingPos);
-        boolean[][] kingMoves = king.possibleMoves();
-
-        for (int i = 0; i < board.getRows(); i++) {
-            for (int j = 0; j < board.getColumns(); j++) {
-                System.out.print(
-                    kingMoves[i][j] ? "X " : "- "
-                );
-            }
-            System.out.println();
-        }
+        
+        ChessMatch match = new ChessMatch();
+        UI.printBoard(
+            match.getPieces()
+        );
     }
 }
